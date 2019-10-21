eacd5b7c2969f9793415183276cd1231652c9503
David Thorpe <djt@mutablelogic.com>
2019-10-21 23:47:17 +1030

    Updated the indexer interface

--- fsindexer.go
+++ fsindexer.go
@@ -14,12 +14,45 @@ import (
 )
 
 ////////////////////////////////////////////////////////////////////////////////
+// TYPES
+
+type FSStatus uint
+
+////////////////////////////////////////////////////////////////////////////////
 // INTERFACES
 
 // FSIndexer defines a file system indexer
 type FSIndexer interface {
 	gopi.Driver
 
-	// Inititate indexing a filesystem
-	Index(string) error
+	// Index inititates indexing a filesystem at
+	// a particular path. The 'watch' argument when
+	// true will watch for updates to the index as they
+	// occur
+	Index(string, bool) (FSIndex, error)
+
+	// Indexes returns all registered indexes and statistics
+	Indexes() []FSIndex
+
+	// Delete will cancel indexing and remove an index
+	Delete(FSIndex) error
 }
+
+// FSIndex represents an index of files or documents
+type FSIndex interface {
+	Id() int64        // Id of the index
+	Name() string     // Name of the index
+	Count() uint64    // Count of the documents or files indexed
+	Status() FSStatus // Status of the index
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+
+const (
+	// FS_STATUS represents the status of an FSIndex objects
+	FS_STATUS_NONE FSStatus = iota
+	FS_STATUS_INDEXING
+	FS_STATUS_IDLE
+	FS_STATUS_WATCHING
+)
